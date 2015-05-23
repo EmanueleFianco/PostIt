567798b98cac073cdf80a935c04584c8dd257f47
Emanuele Fianco <emanuele.fianco@gmail.com>
2015-05-23 17:06:24 +0200

    Creazione classe EGruppo.php

---
+++ Entity/EGruppo.php
@@ -0,0 +1,22 @@
+<?php
+class EGruppo extends ECartella {
+	private $partecipanti = array();
+	
+	public function __construct($_id,$_nome,$_posizione,$_colore,$_contenuto,$_partecipanti) {
+		parent::__construct($_id,$_nome,$_posizione,$_colore,$_contenuto);
+		$this->setPartecipanti($_partecipanti);
+	}
+	
+	public function setPartecipanti($_partecipanti) {
+		$this->partecipanti = $_partecipanti;
+	}
+	
+	public function Push(EPartecipante $_partecipanti) {
+		$this->partecipanti[] = $_partecipanti;
+	}
+	
+	
+	public function getPartecipanti() {
+		return $this->partecipanti;
+	}
+}
