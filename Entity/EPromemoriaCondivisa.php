2ad8987c0f42084ebb5ace9f89042f2c696c7f13
Emanuele Fianco <emanuele.fianco@gmail.com>
2015-05-23 16:59:37 +0200

    Creazione classe EPromemoriaCondivisa.php

---
+++ Entity/EPromemoriaCondivisa.php
@@ -0,0 +1,22 @@
+<?php
+class EPromemoriaCondivisa extends EPromemoria {
+	private $partecipanti = array();
+	
+	public function __construct($_titolo, $_testo, $_immagine, $_posizione, $_colore, $_ora_data_avviso, $_partecipanti) {
+		parent::__construct($_titolo, $_testo, $_immagine, $_posizione, $_colore, $_ora_data_avviso);
+		$this->setPartecipanti($_partecipanti);
+	}
+	
+	public function setPartecipanti($_partecipanti) {
+		$this->partecipanti = $_partecipanti;
+	}
+	
+	public function Push(EPartecipante $_partecipanti) {
+		$this->partecipanti[] = $_partecipanti;
+	}
+	
+	
+	public function getPartecipanti() {
+		return $this->partecipanti;
+	}
+}
