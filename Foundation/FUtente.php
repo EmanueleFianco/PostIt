d63f764f0e248de5b83fd0d53949c371a1c6bdb8
Emanuele Fianco <emanuele.fianco@gmail.com>
2015-05-30 14:49:26 +0200

    Piccole modifiche alle classi Foundation

--- Foundation/FUtente.php
+++ Foundation/FUtente.php
@@ -1,40 +1,27 @@
 <?php
 
-Class FUtente {
+Class FUtente extends Fdb {
 
-	private $db;
-	private $table;
-	private $keydb;
-	private $bind;
-
-public function __construct()
-{   
-    $this->table="utente";
-    $this->keydb="(username,password,immagine,nome,cognome,email,codice_attivazione,stato_attivazione,tipo_utente)";
-    $this->bind="(:username,:password,:immagine,:nome,:cognome,:email,:codice_attivazione,:stato_attivazione,:tipo_utente)";
-    $this->db=USingleton::getInstance('Fdb');
-    
-   
-
-    
-}
-
-public function storeUtente($dati)
-{   
-	$this->db->setParam($this->table,$this->keydb,$this->bind);
-	$this->db->store($dati);
-
-}
-
-public function searchUtente($_value)
-{
-     
-     $this->db->setParam($this->table,"email",":email");
-     return $this->db->loadAsArray("*",$_value);
-
-}
-
-
-
+	public function __construct()
+	{   
+	    $this->table="utente";
+	    $this->keydb="(username,password,immagine,nome,cognome,email,codice_attivazione,stato_attivazione,tipo_utente)";
+	    $this->bind="(:username,:password,:immagine,:nome,:cognome,:email,:codice_attivazione,:stato_attivazione,:tipo_utente)";
+	}
+	
+	public function inserisciUtente($dati)
+	{   
+		$this->db->setParam($this->table,$this->keydb,$this->bind);
+		$this->db->inserisci($dati);
+	
+	}
+	
+	public function carcaUtente($_value)
+	{
+	     
+	     $this->db->setParam($this->table,"email",":email");
+	     return $this->db->loadAsArray("*",$_value);
+	
+	}
 }
 ?>
--- Foundation/Utility/config.inc.php
+++ Foundation/Utility/config.inc.php
@@ -1,8 +1,7 @@
-
 <?php
 $dbms = 'mysql';
 $config[$dbms]['user'] = 'root';
-$config[$dbms]['password'] = '';
+$config[$dbms]['password'] = 'vostrapassword';
 $config[$dbms]['host'] = 'localhost';
 $config[$dbms]['database'] = 'postit';
 ?>
