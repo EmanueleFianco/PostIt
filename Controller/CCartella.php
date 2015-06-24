995146722db943667fee7343f51a4314f0846803
Emanuele Fianco <emanuele.fianco@gmail.com>
2015-06-24 22:47:22 +0200

    Dovrebbe andare

--- Controller/CCartella.php
+++ Controller/CCartella.php
@@ -235,9 +235,9 @@ class CCartella {
 					$tipo_cart = $cart[0]["tipo"];
 					if ($tipo_cart == "privata") {
 						foreach ($note as $key => $value) {
-							var_dump($value);
+							//var_dump($value);
 							if ($valore['condiviso'] == TRUE) {
-								$valore["partecipanti"] = $this->inviaPartecipanti($value['id_nota']);
+								$key["partecipanti"] = $this->inviaPartecipanti($value['id_nota']);
 							}
 						}
 					}
@@ -250,9 +250,9 @@ class CCartella {
 						$tipo_cart = $cart[0]["tipo"];
 						if ($tipo_cart == "privata") {
 							foreach ($note as $key => $value) {
-								var_dump($value);
+								//var_dump($value);
 								if ($value['condiviso'] == TRUE) {
-									$value["partecipanti"] = $this->inviaPartecipanti($value['id_nota']);
+									$key["partecipanti"] = $this->inviaPartecipanti($value['id_nota']);
 								}
 							}
 						}
