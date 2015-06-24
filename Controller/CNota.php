19f4fcfa423da1904e227d6e94637815f138e959
Emanuele Fianco <emanuele.fianco@gmail.com>
2015-06-24 16:16:41 +0200

    Perfezionamento aggiunta nota

--- Controller/CNota.php
+++ Controller/CNota.php
@@ -54,13 +54,14 @@ class CNota {
 			} else {
 				$dati['condiviso'] = FALSE;
 			}
+			$dati['posizione'] = $this->getPosizioneOccupata($dati['id_cartella']);
 			if ($dati['ora_data_avviso'] != "") {
 				$format = 'Y-m-d H:i:s';
 				$data = DateTime::createFromFormat($format,$dati['ora_data_avviso']);
 				if (!$dati['condiviso']) {
 					$CartellaPromemoria = $fcartella->getCartellaByNomeEAmministratore("Promemoria",$session->getValore("email"));
 					$dati['id_cartella'] = $CartellaPromemoria[0]['id'];
-					$max_posizione = $this->getPosizioneOccupata($dati['id_cartella']);
+					$dati['posizione'] = $this->getPosizioneOccupata($dati['id_cartella']);
 					$nota = new EPromemoria($dati['titolo'], $dati['testo'], $dati['posizione'], $dati['colore'], $data);
 				} else {
 					$nota = new EPromemoriaCondiviso($dati['titolo'], $dati['testo'], $dati['posizione'], $dati['colore'],$session->getValore("email"), $data);
@@ -69,7 +70,7 @@ class CNota {
 				if (!$dati['condiviso']) {
 					$CartellaNote = $fcartella->getCartellaByNomeEAmministratore("Note",$session->getValore("email"));
 					$dati['id_cartella'] = $CartellaNote[0]['id'];
-					$max_posizione = $this->getPosizioneOccupata($dati['id_cartella']);
+					$dati['posizione'] = $this->getPosizioneOccupata($dati['id_cartella']);
 					$nota = new ENota($dati['titolo'], $dati['testo'], $dati['posizione'], $dati['colore']);
 				} else {
 					$nota = new ENotaCondivisa($dati['titolo'], $dati['testo'], $dati['posizione'], $dati['colore'],$session->getValore("email"));
